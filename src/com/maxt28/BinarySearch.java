b19771ed4747790f25eec589f596caeec2f70c9a
Maxym Tovstenko <maxym.t28@gmail.com>
2016-12-05 17:05:18 +0200

    If/else statements were replaced into methods with logical name.

--- src/com/maxt28/BinarySearch.java
+++ src/com/maxt28/BinarySearch.java
@@ -6,20 +6,43 @@ public abstract class BinarySearch {
         int leftPosition = 0;
         int rightPosition = array.length - 1;
         int midPosition = array.length / 2;
-        while (array[midPosition] != number) {
-            if ((array[midPosition] < array[rightPosition] && number < array[midPosition])) {
-                rightPosition = midPosition - 1;
-            } else if (array[midPosition] < array[rightPosition] && number > array[midPosition] 
-                    && number > array[rightPosition]) {
-                rightPosition = midPosition - 1;
-            } else if (number < array[midPosition] && number >= array[leftPosition]) {
-                rightPosition = midPosition - 1;
+        return findPosition(leftPosition, rightPosition, midPosition, array, number);
+    }
+
+    private static int findPosition(int leftPos, int rightPos, int midPos, int[] array, int num){
+        while (array[midPos] != num) {
+            boolean rightPartIsSorted = rightPartIsSorted(midPos, rightPos, array);
+            boolean numInLeftPart = numInLeftPart(midPos, array, num);
+            boolean numInRightPart = numInRightPart(midPos, array, num);
+            boolean numIsNotInOffset = numIsNotInOffset(rightPos, array, num);
+            if (rightPartIsSorted && numInLeftPart) {
+                rightPos = midPos - 1;
+            } else if (rightPartIsSorted && numInRightPart && numIsNotInOffset) {
+                rightPos = midPos - 1;
+            } else if (numInLeftPart && numIsNotInOffset) {
+                rightPos = midPos - 1;
             } else {
-                leftPosition = midPosition + 1;
+                leftPos = midPos + 1;
             }
-            midPosition = leftPosition + (rightPosition - leftPosition) / 2;
+            midPos = leftPos + (rightPos - leftPos) / 2;
         }
-        return midPosition;
+        return midPos;
+    }
+    
+    private static boolean numInLeftPart(int midPos, int[] array, int num){
+        return (num < array[midPos]);
+    }
+    
+    private static boolean numInRightPart(int midPos, int[] array, int num){
+        return(num > array[midPos]);
+    }
+    
+    private static boolean rightPartIsSorted(int midPos, int rightPos,int[] array){
+        return(array[midPos] < array[rightPos]);
+    }
+    
+    private static boolean numIsNotInOffset(int rightPos, int[] array, int num){
+        return (num > array[rightPos]);
     }
 }
 
